35e10af836587da815a8917e26da48da076739bd
crccheck <c@crccheck.com>
2013-11-24 17:57:38 -0600

    update to work word by word

--- text-mix.js
+++ text-mix.js
@@ -11,13 +11,24 @@
     return (Math.random() < amount) ? text2[idx]: text1[idx];
   }
 
-  var textMix = function(text1, text2, amount, noise) {
+  var stringMix = function(text1, text2, amount) {
     var new_length = text1.length + Math.floor((text2.length - text1.length) * amount),
         out = '';
     for (var i = 0; i < new_length; i++) {
       out += pick(text1, text2, i, amount);
     }
     return out;
+  };
+
+  var textMix = function(text1, text2, amount) {
+    var words1 = text1.split(' '),
+        words2 = text2.split(' '),
+        out = [];
+    // FIXME handle different words count
+    for (var i = 0; i < words1.length; i++) {
+      out.push(stringMix(words1[i], words2[i], amount))
+    }
+    return out.join(' ');
   };
 
   exports.textMix = textMix;
