18f9ddb240a6e859d4ca88a0bf204dc1609316df
crccheck <c@crccheck.com>
2013-11-24 18:05:01 -0600

    add a different method of mixing numbers

--- text-mix.js
+++ text-mix.js
@@ -1,6 +1,13 @@
 ;(function (exports) {
   'use strict';
 
+  var utils = {
+    // from jQuery, MIT license
+    isNumeric: function( obj ) {
+      return !isNaN( parseFloat(obj) ) && isFinite( obj );
+    }
+  };
+
   var pick = function(text1, text2, idx, amount) {
     // assert idx < Math.max(text1.length, text2.length)
     if (idx >= text1.length) {
@@ -9,7 +16,7 @@
       return text1[idx];
     }
     return (Math.random() < amount) ? text2[idx]: text1[idx];
-  }
+  };
 
   var stringMix = function(text1, text2, amount) {
     var new_length = text1.length + Math.floor((text2.length - text1.length) * amount),
@@ -20,13 +27,22 @@
     return out;
   };
 
+  var numberMix = function(num1, num2, amount) {
+    // FIXME sig digs
+    return Math.round(num1 + (num2 - num1) * amount);
+  };
+
   var textMix = function(text1, text2, amount) {
     var words1 = text1.split(' '),
         words2 = text2.split(' '),
         out = [];
     // FIXME handle different words count
     for (var i = 0; i < words1.length; i++) {
-      out.push(stringMix(words1[i], words2[i], amount))
+      if (utils.isNumeric(words1[i]) && utils.isNumeric(words2[i])) {
+        out.push(numberMix(parseFloat(words1[i]), parseFloat(words2[i]), amount));
+      } else {
+        out.push(stringMix(words1[i], words2[i], amount));
+      }
     }
     return out.join(' ');
   };
