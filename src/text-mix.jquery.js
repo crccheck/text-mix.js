336fc66e655b55827a49df9ffe962890dfdd6294
crccheck <c@crccheck.com>
2015-05-07 22:39:18 -0500

    adapt jquery version to use the more efficient version

--- src/text-mix.jquery.js
+++ src/text-mix.jquery.js
@@ -17,10 +17,11 @@
   $.fn.textMix = function (newText, duration, easing, complete) {
     var self = this,
         oldText = this.text(),
+        mix = textMix.mix(oldText, newText),
         options = {
           easing: 'linear',
           step: function (now, tween) {
-            self.text(textMix.textMix(oldText, newText, now));
+            self.text(mix(now));
           }
         };
     if (duration) {
